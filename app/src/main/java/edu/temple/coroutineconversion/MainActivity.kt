1c630b5e52a9d38ade468cb47af7809137815afe
Liam Cunningham <tuo20284@temple.edu>
2024-03-27 15:23:52 -0400

    Switched thread to coroutine

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,11 +29,13 @@ class MainActivity : AppCompatActivity() {
 
         cakeImageView = findViewById(R.id.imageView)
 
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+            scope.launch {
                 repeat(100) {
                     handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    delay(40)
                 }
             }.start()
         }
